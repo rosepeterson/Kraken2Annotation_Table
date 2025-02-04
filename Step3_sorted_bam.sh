9d76b90da81fc89a9ec9997f150530bf7d67c0e9
Rose Peterson <43347530+rosepeterson@users.noreply.github.com>
2025-02-04 11:52:25 -0500

    Create Step3_sorted_bam.sh

---
+++ Step3_sorted_bam.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#step 3  sorted bam files
+for f in S*/*{HH42,PTVL1}Aligned.out.sam; do
+    in="${f%.sam}.bam"
+    out="${f%.sam}.sorted.bam"
+
+    echo "module load samtools && samtools view -bS $f > $in && samtools sort -o $out $in && samtools index $out" >> sort_sam_2.swarm
+done
