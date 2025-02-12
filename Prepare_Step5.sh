9d15e182bf1ffe00d81cd6f1c1fac4d415e6b119
Rose Peterson <43347530+rosepeterson@users.noreply.github.com>
2025-02-12 18:43:29 -0500

    Update Prepare_Step5.sh

--- Prepare_Step5.sh
+++ Prepare_Step5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #step 5  sorted bam files
-for f in S*/*{HH42,PTlV1,HTlV1,EBV}Aligned.out.sam; do
+for f in S*/*{HH42,PTLV1,HTLV1,EBV}Aligned.out.sam; do
     in="${f%.sam}.bam"
     out="${f%.sam}.sorted.bam"
     echo "module load samtools && samtools view -bS $f > $in && samtools sort -o $out $in && samtools index $out" >> Step5_sort_sam.swarm
