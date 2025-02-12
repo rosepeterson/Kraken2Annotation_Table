b44fae8594e37c4493cd17822efef9cfda716665
Rose Peterson <43347530+rosepeterson@users.noreply.github.com>
2025-02-12 10:11:38 -0500

    Update Prepare_Step1.sh

--- Prepare_Step1.sh
+++ Prepare_Step1.sh
@@ -7,5 +7,5 @@ out=${f%R1_001.fastq.gz}"R1_paired.fastq.gz"
 out2=${f%R1_001.fastq.gz}"R2_paired.fastq.gz"
 outu=${f%R1_001.fastq.gz}"R1_unpaired.fastq.gz"
 outu2=${f%R1_001.fastq.gz}"R2_unpaired.fastq.gz"
-echo "module load trimmomatic/0.39 && java -jar \$TRIMMOJAR PE -phred33 $f $in2 $out $outu $out2 $outu2" >> Step1_filter_trimm.swarm
+echo "module load trimmomatic/0.39 && java -jar \$TRIMMOJAR PE -phred33 $f $in2 $out $outu $out2 $outu2 ILLUMINACLIP:/usr/local/apps/trimmomatic/0.39/adapters/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36" >> Step1_filter_trimm.swarm
 done
