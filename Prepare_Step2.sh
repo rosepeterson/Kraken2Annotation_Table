37a9204deea2fe5631186e129097f3239dd11fa1
Rose Peterson <43347530+rosepeterson@users.noreply.github.com>
2025-05-01 11:52:38 -0400

    Update Prepare_Step2.sh

--- Prepare_Step2.sh
+++ Prepare_Step2.sh
@@ -3,5 +3,6 @@ for f in */*R1_paired.fastq.gz;
 do
 in=${f%R1_paired.fastq.gz}"R[1-2]_paired.fastq.gz"
 out=${f%R1_paired.fastq.gz}".txt"
-echo "module load kraken/2.1.2 && kraken2 --db /fdb/kraken/20220803_standard_kraken2  --output $out --use-names   --gzip-compressed   $in" >> Step2_run_Kraken2.swarm
+rep=${f%R1_paired.fastq.gz}".report"
+echo "module load kraken/2.1.2 && kraken2 --db /fdb/kraken/20220803_standard_kraken2  --output $out   --report $rep --use-names   --gzip-compressed   $in" >> Step2_run_Kraken2_report.swarm
 done
